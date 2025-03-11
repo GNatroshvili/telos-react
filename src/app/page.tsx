b2912a513c975c32c37eb11bc3cc49c24ed25f2f
GNatroshvili <gigi.natroshvili@gmail.com>
2025-03-12 00:49:28 +0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
