ebf4f84d7f20a31652e44d0d5edf82205ec02faa
GNatroshvili <gigi.natroshvili@gmail.com>
2025-03-12 11:51:15 +0400

    add Hero and FeaturedProducts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/hero/Hero";
+import FeauredProducts from "./components/featuredProducts/FeauredProducts";
 
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
+      <Hero/>
+      <FeauredProducts/>
       <Footer/>
     </div>
   );
